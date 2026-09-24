3144413a5fc250e8df416fd0aad13c614ba7fd68
Kavinkumar <kavinkumars489@gmail.com>
2026-09-24 21:03:18 +0530

    Time: 1 ms (99.69%), Space: 45.4 MB (77.88%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -4,7 +4,8 @@ class Solution {
         for(int i = 0 ; i < n ; i++){
             int sum = 0;
             while(nums[i] > 0){
-                sum += nums[i] % 10;
+                int mod = nums[i] % 10;
+                sum += mod;
                 nums[i] /= 10;
             }
             if(sum == i){
